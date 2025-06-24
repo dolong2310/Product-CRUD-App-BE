3cfe3a4fa559604a27b5d82fa97d677b0915e4b0
longdo <dolong2310@gmail.com>
2025-06-24 18:55:53 +0700

    initial commit

---
+++ src/controllers/account.controller.ts
@@ -0,0 +1,15 @@
+import prisma from '@/database'
+import { UpdateMeBodyType } from '@/schemaValidations/account.schema'
+
+export const updateMeController = async (accountId: number, body: UpdateMeBodyType) => {
+  const account = prisma.account.update({
+    where: {
+      id: accountId
+    },
+    data: {
+      name: body.name,
+      avatar: body.avatar
+    }
+  })
+  return account
+}
